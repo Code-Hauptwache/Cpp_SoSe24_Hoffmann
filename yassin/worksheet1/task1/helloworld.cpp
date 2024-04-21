ee37aa97716c26ed1217fbf0c95c24e00746df00
Yassin561 <yassin.elamraoui@hotmail.com>
2024-04-21 21:52:44 +0200

    Task1

---
+++ yassin/worksheet1/task1/helloworld.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include <iostream>
+
+int main(void)
+{
+    int tag = 16;
+    int monat = 4;
+    int jahr = 2024;
+
+    printf("Hello World! am %i.%i.%i\n", tag, monat, jahr);
+
+    /* std::cout << "Hello World!" << " am "
+    << tag << "." << monat << "." << jahr << std::endl; */
+
+    return 0;
+}
+
+/* cout character output*/ 
+
+
