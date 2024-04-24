1ac90324a01cef49bc25551473e96755b0898004
zakaBouj <zb2003boujana@gmail.com>
2024-04-24 18:58:44 +0200

    worksheet 2

---
+++ zaka/worksheet2/task3/faculty.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+long fak(unsigned long n);
+
+int main()
+{
+	long n;
+	long result;
+		
+	cin >> n;
+	
+	result = fak(n);
+	
+	cout << n << "! entspricht " << result << endl;
+	
+	cout.put('\n');
+	cout << "Achtung: Dieser Code könnte für n >= 13 ungenau werden, da die Fakultätswerte den Wertebereich eines 32-Bit 'long' überschreiten können." << endl;
+	
+	return 0;
+}
+
+long fak(unsigned long n)
+{
+	if (n <= 1)
+		return n;
+
+	return n *= fak(n -1);12
+}
