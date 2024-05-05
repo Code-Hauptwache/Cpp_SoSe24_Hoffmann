5cb179add6e2e053ea2300755091ceb2b4cf8740
Yassin561 <yassin.elamraoui@hotmail.com>
2024-05-05 18:43:50 +0200

    refrehsed

--- yassin/worksheet3/task3/aufgabe3.cpp
+++ yassin/worksheet3/task3/aufgabe3.cpp
@@ -1,33 +1,43 @@
 #include <iostream>
+
 using namespace std;
 
-namespace MySwap {
-    bool swap(int& num, int& num2) {
-        if (num > 0 && num2 > 0) {
-            int temp = num;
-            num = num2;
-            num2 = temp;
-            return true;
-        }
-        return false;
-    }
+namespace MySwap 
+{
+	void swap(int& num, int& num2, bool checkSign = true) 
+	{
+		if(checkSign)
+		{
+			if((num >= 0 && num2 >= 0)||( num < 0 && num2 < 0))
+			{
+				int temp = num;
+				num = num2;
+				num2 = temp;		
+			}
+		}
+		else
+		{
+			int temp = num;
+				num = num2;
+				num2 = temp;
+		}
+	}	
 }
-
 int main() {
     int zahl1, zahl2;
     
-    do {
-        cout << "Geben Sie 2 positive Zahlen ein: " << endl;
-        cin >> zahl1 >> zahl2;
-    } while (zahl1 < 0 || zahl2 < 0);
+
+	cout << "Gebe 2 Zahlen ein: " << endl;
+	cin >> zahl1 >> zahl2;
+    
     
     cout << "Eingangswerte: " << zahl1 << " " << zahl2 << endl;
     
-    if (MySwap::swap(zahl1, zahl2)) {
+     MySwap::swap(zahl1,zahl2);
         cout << "Ausgangswerte: " << zahl1 << " " << zahl2 << endl;
-    } else {
-        cout << "Mindestens eine der eingegebenen Zahlen ist nicht positiv." << endl;
-    }
+    
+     MySwap::swap(zahl1,zahl2,false);
+		cout << "Ausgangswerte: " << zahl1 << " " << zahl2  << endl;
 
     return 0;
 }
