4aef29216e173553c83f55840d06817cefd9bce2
Yassin561 <yassin.elamraoui@hotmail.com>
2024-05-16 21:53:49 +0200

    1/5

---
+++ yassin/worksheet4/array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{	
+	array <int, 5> array_float{5,-2,9,1,0};
+	array <int , 5> arrCopy = arr;
+	sort(arrCopy.begin(), arrCopy.end());
+	
+	
+	
+	return 0;
+}	
