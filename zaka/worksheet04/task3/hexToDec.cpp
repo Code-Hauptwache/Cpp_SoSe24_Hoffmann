86557a446be7912de3c5f5c15062bee4996f4e13
zakaBouj <zb2003boujana@gmail.com>
2024-05-16 10:21:06 +0200

    wrksht04 tsk3

---
+++ zaka/worksheet04/task3/hexToDec.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+int todec(string str, int i);
+int hexToDec(string str);
+
+int main()
+{
+	string hexNum = "ff03"
+	
+	return 0;
+}
+
+int todec(string str, int i)
+{
+}
+
+int hexToDec(string str)
+{
+}
