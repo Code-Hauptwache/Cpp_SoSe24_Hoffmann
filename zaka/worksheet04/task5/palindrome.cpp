b26c145778713b560fdc13aa2c7532d0f753eb04
zaka <zb2003boujana@gmail.com>
2024-05-16 23:48:59 +0200

    wrksht4 tsk5

--- zaka/worksheet04/task5/palindrome.cpp
+++ zaka/worksheet04/task5/palindrome.cpp
@@ -1,20 +1,57 @@
 #include <iostream>
+#include <string>
+#include <algorithm> // For transform function
 
 using namespace std;
 
-bool palindrom(string& s);
+namespace myString {
+    void reverse(string& str);
+    bool palindrom(string& s);
+}
+
+// Function to convert string to lower case
+string toLowerCase(string str) {
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    return str;
+}
 
 int main()
 {
     string str;
+    string reversedStr;
 
-    cout << "Enter a word/string to: ";
+    cout << "Please enter a string: ";
     cin >> str;
+
+    if (myString::palindrom(str)) {
+        cout << str << " is a palindrome." << endl;
+    } else {
+        cout << str << " is not a palindrome." << endl;
+    }
 
     return 0;
 }
 
-bool palindrom(string& s)
-{
+namespace myString {
+    void reverse(string& str)
+    {
+        int strLen = str.length();
+        string reverseStr = "";
 
+        for (int i = 0; i < strLen; ++i) {
+            reverseStr += str[strLen - 1 - i];
+        }
+
+        str = reverseStr;
+    }
+
+    bool palindrom(string& s)
+    {
+        string originalStr = toLowerCase(s);
+        string reversedStr = originalStr;
+
+        reverse(reversedStr);
+
+        return originalStr == reversedStr;
+    }
 }
