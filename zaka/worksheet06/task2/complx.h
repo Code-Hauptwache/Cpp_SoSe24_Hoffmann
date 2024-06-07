9e090c01a57662978a6e0ae53d6b926d0afa4310
zakaBouj <zb2003boujana@gmail.com>
2024-06-07 11:29:00 +0200

    task2 & extra Task

---
+++ zaka/worksheet06/task2/complx.h
@@ -0,0 +1,34 @@
+#include <cmath>
+
+#ifndef COMPLX_H
+#define COMPLX_H
+
+namespace ToComplx{
+  class Complx {
+    private:
+      double re;
+      double im;
+    public:
+      explicit Complx(double r, double i) : re{r}, im{i} { }; // Konstruktor
+      Complx(double r) : re{r}, im{0.0} { };         // Konstruktor mit im=0
+      explicit Complx() : re{0.0}, im{0.0} { };               // Konstruktor re=im=0
+      ~Complx() {std::cout << "Objekt Zerstoert" << std::endl ;};                                  // Destruktor
+//      Complx(const Complx&) { };                     // Copy Konstruktor
+      Complx(const Complx& z) : re(z.re), im(z.im) { }; // Copy Konstruktor
+      
+      double getReal() const { return re; };            // Getter -> const
+      double getImag() const { return im; };
+      void setReal(double r) { re = r; }                // Setter
+      void setImag(double i) { im = i;  }
+
+ // Eigene Memberfunktionen: ComplxAdd, ComplxSub, ComplxMult => Return by Value
+
+      Complx operator+(Complx z) { return Complx( re += z.re, im += z.im ); }
+      Complx operator-(Complx z) { return Complx( re -= z.re, im -= z.im ); }
+      Complx operator*(Complx z) { return Complx( re*z.re - im*z.im, re*z.im + im*z.re ); }
+      Complx operator|(Complx z) { return Complx( re = sqrt(re*re + im*im); }
+      
+   };
+};
+#endif // Datei: complx.h
+
