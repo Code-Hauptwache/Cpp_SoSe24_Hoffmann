aa3ced150e4c9c533005b971f0c60b20b2036986
zaka <103366844+zakaBouj@users.noreply.github.com>
2024-07-31 19:50:42 +0200

    Complete manageDataObj.cpp

--- zaka/examPrep/task1/manageDataObj.cpp
+++ zaka/examPrep/task1/manageDataObj.cpp
@@ -8,4 +8,123 @@
  * 
  */
 
-// TBD: manageDataObj.cpp
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <limits>
+#include <vector>
+
+using namespace std;
+
+void addDataObj(vector<string> &vector);
+void deleteDataObj(vector<string> &vector);
+void showDataObjs(vector<string> &vector, bool &sorted);
+
+int main()
+{
+    int choice = 1;
+    bool sortState = false;
+    vector<string> vec;
+
+    while (1)
+    {
+        cout << "Pick a option" << endl;
+        cout << "1. Add  data set" << endl;
+        cout << "2. Delete data set" << endl;
+        cout << "3. Show data sets" << endl;
+        cout << "4. End program" << endl;
+
+        do
+        {
+            if (choice < 1 || choice > 4 || cin.fail())
+            {
+                cerr << "Invalide iput. Please try again." << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+
+            cout << "Option: ";
+            cin >> choice;
+        } while (choice < 1 || choice > 4 || cin.fail());
+        
+        switch (choice)
+        {
+            case (1):
+                addDataObj(vec);
+                break;
+            case (2):
+                deleteDataObj(vec);
+                break;
+            case (3):
+                showDataObjs(vec, sortState);
+                break;
+            case (4):
+                return 0;
+            default:
+                break;
+        }
+    }
+
+    return 0;
+}
+
+void addDataObj(vector<string> &vector)
+{
+    string temp;
+
+    cout << "Enter content for new data set: ";
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin, temp);
+    vector.push_back(temp);
+}
+
+void deleteDataObj(vector<string> &vector)
+{
+    if (vector.size() == 0)
+    {
+        cout << "There is nothing to be done." << endl;
+        return;
+    }
+    else
+    {
+        int choice;
+        size_t vectorSize = vector.size();
+
+        do
+        {
+            if (choice < 0 || choice > vectorSize || cin.fail())
+            {
+                cerr << "\nInvalide input. Please try again." << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+
+            cout<< "Enter data set to be deletet (1 to " << vectorSize << ", 0 to cancel)";
+            cin >> choice;   
+        } while (choice < 0 || (size_t)choice > vectorSize || cin.fail());
+
+        if (choice == 0) return;
+
+        cout << "Deleting data set at index " << choice << ": " << vector[(size_t)choice - 1] << endl;
+        vector.erase(vector.begin() + (size_t)choice - 1);
+    }
+}
+
+void showDataObjs(vector<string> &vector, bool &sorted)
+{
+    size_t vectorSize = vector.size();
+    
+    if (vectorSize == 0)
+    {
+        cout << "There is not data set to show." << endl;
+        return;
+    }
+
+    if (!sorted) sort(vector.begin(), vector.end());
+
+    for (size_t i = 0; i < vectorSize; i++)
+    {
+        cout << i + 1 << ". " << vector[i] << endl;
+    }
+}
